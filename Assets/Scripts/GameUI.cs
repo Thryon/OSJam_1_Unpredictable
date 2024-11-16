1470743261324dbcb2ead2dee6c3a26c2dfd1e52
RomainBrissaud <romain.brissaud@gmail.com>
2024-11-16 18:49:58 +0100

    Added UI banner animation at start

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -5,15 +5,9 @@ using UnityEngine;
 public class GameUI : MonoBehaviour
 {
     [SerializeField] private GameObject _bannerRoundInfo;
-
-
+    
     public void Start()
     {
         _bannerRoundInfo.transform.localScale = Vector3.zero;
-    }
-
-    public void AnimateBanner()
-    {
-        _bannerRoundInfo.transform.DOScale(Vector3.one, 1f);
     }
 }
