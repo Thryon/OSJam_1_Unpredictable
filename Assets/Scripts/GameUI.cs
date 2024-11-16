efcb287e5b67b2fa3edec4aa6a8369050d9a227a
RomainBrissaud <romain.brissaud@gmail.com>
2024-11-16 19:19:44 +0100

    Added tutorial and text animator

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class GameUI : MonoBehaviour
 {
     [SerializeField] private GameObject _bannerRoundInfo;
+    [SerializeField] private GameObject _tutorialInfo;
     
     public void Start()
     {
+        GlobalEvents.OnInputBuffered.AddListener(DisableTutorialText);
         _bannerRoundInfo.transform.localScale = Vector3.zero;
+    }
+
+    private void DisableTutorialText((int, InputManager.EInputType) arg0)
+    {
+        _tutorialInfo.GetComponent<CanvasGroup>()?.DOFade(0f, 0.5f);
     }
 }
