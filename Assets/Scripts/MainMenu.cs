05b12120595c9444e4c068e99973429e291a0e59
RomainBrissaud <romain.brissaud@gmail.com>
2024-11-17 12:04:09 +0100

    Added differents scene and prefabized everything

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,6 +8,19 @@ public class MainMenu : MonoBehaviour
     [Header("References")]
     [SerializeField] private Button _playButton;
     [SerializeField] private Button _quitButton;
+    [SerializeField] private Image _imagePreview;
+
+    [Header("Level Preview")] 
+    [SerializeField] private Image _previewLvl1;
+    [SerializeField] private Image _previewLvl2;
+    [SerializeField] private Image _previewLvl3;
+
+    [Header("SceneSelected")] 
+    [SerializeField] private int _buildIndexFirstLevel;
+    [SerializeField] private int _buildIndexSecondLevel;
+    [SerializeField] private int _buildIndexThirdLevel;
+
+    private int _indexOfPlaySceneSelected;
 
     private void Start()
     {
@@ -22,5 +36,24 @@ public class MainMenu : MonoBehaviour
     private void QuitButtonPressed()
     {
         Application.Quit();
+    }
+
+    public void SliderChangeLevelPreview(Single index)
+    {
+        switch (index)
+        {
+            case 0:
+                _imagePreview = _previewLvl1;
+                _indexOfPlaySceneSelected = _buildIndexFirstLevel;
+                break;
+            case 1:
+                _imagePreview = _previewLvl2;
+                _indexOfPlaySceneSelected = _buildIndexSecondLevel;
+                break;
+            case 2:
+                _imagePreview = _previewLvl3;
+                _indexOfPlaySceneSelected = _buildIndexThirdLevel;
+                break;
+        }
     }
 }
