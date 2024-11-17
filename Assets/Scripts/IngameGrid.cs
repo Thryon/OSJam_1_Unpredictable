830306f3d119f93f9b33ec488e06fcff72dea22d
Sébastien DAMIENS <sdamiens@oldskullgames.com>
2024-11-17 12:46:43 +0100

    Instanciate cell with prefab utility

--- Assets/Scripts/IngameGrid.cs
+++ Assets/Scripts/IngameGrid.cs
@@ -145,7 +145,7 @@ public class IngameGrid : MonoBehaviour
 
     private IngameCell SpawnCell(int x, int y)
     {
-        IngameCell cell = (IngameCell)Instantiate(_ingameCellPrefab, transform);
+        IngameCell cell = (IngameCell)PrefabUtility.InstantiatePrefab(_ingameCellPrefab, transform);
         cell.transform.localPosition = new Vector3(x * cellsDistance, 0f, y * cellsDistance);
         cell.SetPositionInGrid(x, y);
         cell.name = "Cell_" + x + "_" + y;
